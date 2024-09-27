74436ff7cac8d0e1116d3f28d0aa6a49f342d035
Vini <134401761+vini-destruidor19@users.noreply.github.com>
2024-09-27 07:42:24 -0300

    Add files via upload

--- graficos/Dados.js
+++ graficos/Dados.js
@@ -5,13 +5,13 @@ async function visualizarInfos() {
     const dados = await res.json()
 
     const CelularesMaisVendidos = dados[0].modelo
-    const NumerosDeVendas = dados[0].votos
+    const NumerosDeVendas = dados[0].votos/1e6
     const Percentual = dados[0].percentual
 
     console.log(dados[0].modelo) 
     const paragrafo = document.createElement("p")
     paragrafo.classList.add("caixa-grafico__texto")
-    paragrafo.innerHTML = `Em busca de descobrir os celulares mais comprados, foi feito uma pesquisa usando inteligencia artificial, foi possivel concluir que o celuar Apple iPhone 14 ficou em primeiro lucar, o principal comnetario a respeito do celuar foi: iPhone 14 é o meu preferido com um designer impecável`
+    paragrafo.innerHTML = `Em busca de descobrir os celulares mais comprados, foi feito uma pesquisa usando inteligencia artificial, foi possivel concluir que o <span>${CelularesMaisVendidos}</span> ficou em primeiro lucar com <span>${NumerosDeVendas}</span>, o principal comnetario a respeito do celuar foi: iPhone 14 é o meu preferido com um designer impecável`
     const caixa = document.getElementById("caixa-grafico")
     caixa.appendChild(paragrafo)
 }
