72f0d62bffc06ef5087fe6dae4ef26ad9fa4959e
Vini <134401761+vini-destruidor19@users.noreply.github.com>
2024-10-03 12:13:53 -0300

    Add files via upload

--- graficos/graficobarra.js
+++ graficos/graficobarra.js
@@ -1,51 +1,10 @@
-import { pegarCSS, configuraEixo } from "./comum.js"
+const url ='https://raw.githubusercontent.com/vini-destruidor19/API-ex-vini/refs/heads/main/entrevista.json'
 
-async function criarGraficoBarra(){
-    const url = 'https://raw.githubusercontent.com/vini-destruidor19/API-ex-vini/refs/heads/main/Barra.json'
+async function CriarGraficoBarra(){
+    const url = 'https://raw.githubusercontent.com/vini-destruidor19/API-ex-vini/refs/heads/main/entrevista.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const celulares = Object.keys(dados)
-    const vendas = Object.values(dados)
-
-
-    const data = [
-        {
-            x: celulares,
-            y: vendas,
-            type:'bar',
-            marker: {
-                color: pegarCSS('--vermelho')
-            }
-        }
-    ]
-    const layout = {
-        plot_bgcolor: pegarCSS('--preto'),
-        paper_bgcolor: pegarCSS('--preto'),
-        title:{
-            text:'os celulares mais vendido no mundo',
-            font: {
-                color: pegarCSS('--branco'),
-                family: pegarCSS('--fonte-titulo'),
-                size: 50
-            }
-        },
-        xaxis:{
-            tickfont: configuraEixo,
-            tickangule: 0
-        },
-        yaxis:{
-            tickfont: configuraEixo,
-            tickangule: 45
-        }
-    }
-    
-
-    const grafico = document.createElement('div')
-    grafico.className = "grafico"
-    document.getElementById('caixa-grafico').appendChild(grafico)
-    Plotly.newPlot(grafico, data, layout)
-
+    console.log(dados)
 }
 
-
-criarGraficoBarra()
+CriarGraficoBarra()
